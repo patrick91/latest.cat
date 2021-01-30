f99fba30198a7454f74fb15bf39fe1fad74c7dad
Patrick Arminio <patrick.arminio@gmail.com>
2021-01-30 10:44:46 +0100

    Fix wrong import

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from starlette.routing import Route
 from data.config import database
 from pages.home import homepage
 from pages.search import search
-from pages.version import version
+from pages.version import fetch_latest
 
 app = Starlette(
     debug=True,
     routes=[
         Route("/", homepage),
         Route("/search", search),
-        Route("/{slug}", version),
+        Route("/{slug}", fetch_latest),
     ],
     on_startup=[database.connect],
     on_shutdown=[database.disconnect],
