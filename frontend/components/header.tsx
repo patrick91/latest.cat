1aea2fc55490ba25053b36d35142f383051bb0b9
Patrick Arminio <patrick.arminio@gmail.com>
2022-03-31 15:41:15 -0500

    Add link

--- frontend/components/header.tsx
+++ frontend/components/header.tsx
@@ -1,11 +1,10 @@
+import Link from "next/link";
 import { useCallback } from "react";
 import { Logo } from "./logo";
 import { Toggle } from "./toggle";
 
 export const Header = () => {
   const toggleDarkMode = useCallback((value: boolean) => {
-    console.log("Toggle Dark Mode");
-    // change class of html element
     if (value) {
       document.documentElement.classList.add("dark");
     } else {
@@ -15,7 +14,11 @@ export const Header = () => {
 
   return (
     <header className="max-w-7xl mx-auto px-10 py-5 flex justify-between align-middle">
-      <Logo />
+      <Link href="/">
+        <a>
+          <Logo />
+        </a>
+      </Link>
 
       <div className="text-2xl">🐾</div>
 
