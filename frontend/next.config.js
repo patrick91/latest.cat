21c1260439ecf3b8bf32debcce4663cf909de690
Patrick Arminio <patrick.arminio@gmail.com>
2022-04-04 22:40:19 -0500

    Add proxy to GraphQL API

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
 
-module.exports = nextConfig
+  async rewrites() {
+    return [
+      {
+        source: "/graphql",
+        destination: "https://latest-cat-65bwarhayq-ew.a.run.app/graphql",
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
