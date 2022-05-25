a12ad84082956e19d8d8e399ee20d496c78d30c4
Thomas Eckert <thomas.james.eckert@gmail.com>
2022-05-24 22:29:38 -0400

    Fix mobile display

--- frontend/components/command.tsx
+++ frontend/components/command.tsx
@@ -1,14 +1,14 @@
 export const Command = ({ text }: { text: string }) => (
   <div className="flex flex-col sm:flex-row items-baseline justify-between">
-    <div className="mb-4 flex overflow-auto">
-      <span className="mr-4 select-none">$</span>
+    <div className="mb-4 flex overflow-auto text-[1rem] sm:text-[1em]">
+      <span className="mr-2 sm:mr-4 select-none">$</span>
       <pre>
         <code>{text}</code>
       </pre>
     </div>
     <button
       onClick={() => navigator.clipboard.writeText(text)}
-      className="px-2 py-1 font-heading font-medium rounded-full hover:bg-gray text-black transition active:bg-dark-gray active:text-white"
+      className="px-2 py-1 font-heading font-medium rounded-full hidden sm:inline hover:bg-gray text-black transition active:bg-dark-gray active:text-white"
     >
       Copy
     </button>
