85ccf52400752eecbc9bae5b40b5c40485ae2b96
Patrick Arminio <patrick.arminio@gmail.com>
2022-07-13 17:15:35 +0100

    Hack

--- frontend/components/marquee.tsx
+++ frontend/components/marquee.tsx
@@ -8,12 +8,12 @@ export const Marquee = () => (
       "
   >
     <ReactMarquee reverse pauseOnHover duration={40000}>
-      <div className="mr-10">• Python 3.10.2 Released</div>
-      <div className="mr-10">• Python 3.10.2 Released</div>
-      <div className="mr-10">• Python 3.10.2 Released</div>
-      <div className="mr-10">• Python 3.10.2 Released</div>
-      <div className="mr-10">• Python 3.10.2 Released</div>
-      <div className="mr-10">• Python 3.10.2 Released</div>
+      <div className="mr-10">• Python 3.10.5 Released</div>
+      <div className="mr-10">• Python 3.10.5 Released</div>
+      <div className="mr-10">• Python 3.10.5 Released</div>
+      <div className="mr-10">• Python 3.10.5 Released</div>
+      <div className="mr-10">• Python 3.10.5 Released</div>
+      <div className="mr-10">• Python 3.10.5 Released</div>
     </ReactMarquee>
   </div>
 );
