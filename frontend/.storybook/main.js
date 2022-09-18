b901bc1b22a14dce772f4aa81ed72e4b32d9d58c
Patrick Arminio <patrick.arminio@gmail.com>
2022-09-18 09:33:35 +0100

    Fix storybook

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -8,7 +8,14 @@ module.exports = {
     "../components/**/*.stories.@(js|jsx|ts|tsx)",
   ],
   addons: [
-    "@storybook/addon-postcss",
+    {
+      name: "@storybook/addon-postcss",
+      options: {
+        postcssLoaderOptions: {
+          implementation: require("postcss"),
+        },
+      },
+    },
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
