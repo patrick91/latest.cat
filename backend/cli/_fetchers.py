e4d7fc77714327d4424e5a52d02444b755b52738
Patrick Arminio <patrick.arminio@gmail.com>
2022-10-22 23:23:07 -0700

    Pagination

--- backend/cli/_fetchers.py
+++ backend/cli/_fetchers.py
@@ -6,9 +6,13 @@ from ._config import github_token
 from ._software import Software
 
 FETCH_TAGS_QUERY = """
-query FetchTags($owner: String!, $name: String!) {
+query FetchTags($owner: String!, $name: String!, $before: String) {
     repository(owner: $owner, name: $name) {
-        refs(refPrefix: "refs/tags/", last: 100) {
+        refs(refPrefix: "refs/tags/", last: 100, before: $before) {
+            pageInfo {
+                hasPreviousPage
+                startCursor
+            }
             nodes {
                 name
             }
@@ -22,24 +26,40 @@ async def fetch_tags_from_github(repository: str) -> AsyncGenerator[str, None]:
     owner, name = repository.split("/")
 
     async with httpx.AsyncClient() as client:
-        response = await client.post(
-            "https://api.github.com/graphql",
-            headers={"Authorization": f"bearer {github_token}"},
-            json={
-                "query": FETCH_TAGS_QUERY,
-                "variables": {
-                    "owner": owner,
-                    "name": name,
+        has_previous_page = True
+        before = None
+
+        # TODO: optimize this in future
+
+        while has_previous_page:
+            response = await client.post(
+                "https://api.github.com/graphql",
+                headers={"Authorization": f"bearer {github_token}"},
+                json={
+                    "query": FETCH_TAGS_QUERY,
+                    "variables": {
+                        "owner": owner,
+                        "name": name,
+                        "before": before,
+                    },
                 },
-            },
-        )
+            )
 
-        response.raise_for_status()
+            response.raise_for_status()
+            content = response.json()
 
-    tags = response.json()["data"]["repository"]["refs"]["nodes"]
+            if errors := content.get("errors"):
+                raise ValueError(errors)
 
-    for tag in tags:
-        yield tag["name"]
+            data = content["data"]
+            tags = data["repository"]["refs"]["nodes"]
+            has_previous_page = data["repository"]["refs"]["pageInfo"][
+                "hasPreviousPage"
+            ]
+            before = data["repository"]["refs"]["pageInfo"]["startCursor"]
+
+            for tag in tags:
+                yield tag["name"]
 
 
 async def fetch_versions(
