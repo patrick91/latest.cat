dfb332b8c514396b421b22e3ee55605414549b21
Patrick Arminio <patrick.arminio@gmail.com>
2023-04-09 22:12:07 +0100

    Update

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,25 +1,19 @@
 module.exports = {
-  stories: [
-    "../components/*.stories.mdx",
-    "../components/*.stories.@(js|jsx|ts|tsx)",
-    "../components/**/story.mdx",
-    "../components/**/story.@(js|jsx|ts|tsx)",
-    "../components/**/*.stories.mdx",
-    "../components/**/*.stories.@(js|jsx|ts|tsx)",
-  ],
-  addons: [
-    {
-      name: "@storybook/addon-postcss",
-      options: {
-        postcssLoaderOptions: {
-          implementation: require("postcss"),
-        },
-      },
-    },
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-  ],
-  framework: "@storybook/react",
+  stories: ["../components/*.stories.mdx", "../components/*.stories.@(js|jsx|ts|tsx)", "../components/**/story.mdx", "../components/**/story.@(js|jsx|ts|tsx)", "../components/**/*.stories.mdx", "../components/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [{
+    name: "@storybook/addon-postcss",
+    options: {
+      postcssLoaderOptions: {
+        implementation: require("postcss")
+      }
+    }
+  }, "@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-mdx-gfm"],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {}
+  },
   staticDirs: ["../public"],
+  docs: {
+    autodocs: true
+  }
 };
