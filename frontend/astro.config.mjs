f10eea1d83a9245753f3ca36f988e373df7a1ed9
Patrick Arminio <patrick.arminio@gmail.com>
2023-12-01 18:09:11 +0100

    Add adapter

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  adapter: vercel()
 });
