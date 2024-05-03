3a53fb80466621ff477a8c8bcbbed95c2ab99932
Patrick Arminio <patrick.arminio@gmail.com>
2024-05-03 13:27:00 +0200

    Add theme color

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
-
 import react from "@astrojs/react";
+
+import metaTags from "astro-meta-tags";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), metaTags()],
   adapter: vercel()
 });
